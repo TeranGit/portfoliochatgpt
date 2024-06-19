b70858c398776319105377b151fc4feebe647f2a
teran <frubengarcia@gmail.com>
2024-06-18 19:27:03 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name && email && message) {
+        alert('Formulario enviado con éxito.\nNombre: ' + name + '\nCorreo Electrónico: ' + email + '\nMensaje: ' + message);
+    } else {
+        alert('Por favor, completa todos los campos del formulario.');
+    }
+});
